c7334c2532b0f8535d50780bf04f61a9e6b3ecb3
Igor Yunenko <zigoryun@gmail.com>
2020-01-18 15:46:58 +0200

    first commit

---
+++ js-hw1/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// TASK_1
+const name = 'Генератор защитного поля';
+let price = 1000;
+
+console.log(`Выбран «${name}», цена за шутку ${price} кредитов.`);
+
+price = 2000;
+console.log(`Выбран «${name}», цена за шутку ${price} кредитов.`);
+
+// TASK_2
+const total = 100;
+const ordered = 50;
+
